1bad6f7bedd846d311e7b4d6dac2be8dc123652d
Mehmet <mehmetk6158@gmail.com>
2021-11-17 20:16:02 -0500

    add create action to backend-api

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,16 @@
 class KittensController < ApplicationController
+  def index
+    kittens = Kitten.all
+    render json:kittens
+  end
+  def create
+    kitten = Kitten.new(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age]
+    )
+    kitten.save
+    render json:kitten
+  
+  end
 end
